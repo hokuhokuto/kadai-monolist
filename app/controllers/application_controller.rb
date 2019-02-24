ac6c183f52a5a26323cc0b3daea20ca021a0af14
hokuto.nakata <headupdisplay000@gmail.com>
2019-02-24 11:05:49 +0000

    rakuten api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-  include SessionHelper
+  include SessionsHelper
   
-  def requre_user_logged_in
+  def require_user_logged_in
     unless logged_in?
       redirect_to login_url
     end
