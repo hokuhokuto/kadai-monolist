f38c32bc3b38e32364615118f42569f5e02640d3
hokuto.nakata <headupdisplay000@gmail.com>
2019-02-24 03:48:02 +0000

    user login

---
+++ app/models/item.rb
@@ -0,0 +1,6 @@
+class Item < ApplicationRecord
+  validates :code, presence: true, length: { maximum: 255 }
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :url, presence: true, length: { maximum: 255 }
+  validates :image_user, presence: true, length: { maximum: 255 }
+end
